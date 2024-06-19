1356b445b6f7131ba311b54cd895535a7f45b3df
qicuizhi <3084685562@qq.com>
2024-06-19 12:03:15 +0800

    首页-轮播图

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,8 +1,25 @@
 <script setup lang="ts">
-//
+import { ref } from 'vue'
+import CustomNavbar from './components/CustomNavbar.vue'
+import { getHomeBannerAPI } from '@/services/home.ts'
+import type { BannerItem } from '@/types/home'
+import { onLoad } from '@dcloudio/uni-app'
+
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  // console.log(res)
+  bannerList.value = res.result
+}
+onLoad(() => {
+  getHomeBannerData()
+})
 </script>
 
 <template>
+  <CustomNavbar />
+  <!-- 不知为何类型仍为any -->
+  <XtxSwiper :list="bannerList" />
   <view class="index">index</view>
 </template>
 
